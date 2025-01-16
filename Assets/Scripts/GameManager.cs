0512362e02fcf9a05687f6b142dd070245c0fdfa
Mauriiicio <132005678+Pedro3DS@users.noreply.github.com>
2025-01-16 19:14:58 -0400

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,8 @@
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -12,23 +12,33 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         GetCommands();
+        // __GetEnemySequence();
     }
     public void GetCommands()
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow)){
             Debug.Log("A");
+            __GetEnemySequence('A');
         }
         else if(Input.GetKeyDown(KeyCode.UpArrow)){
             Debug.Log("B");
+            __GetEnemySequence('B');
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow)){
             Debug.Log("X");
+            __GetEnemySequence('X');
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow)){
             Debug.Log("Y");
+            __GetEnemySequence('Y');
         }
     }
-    private void __GetEnemySequence(){
-        
+    private void __GetEnemySequence(char buttonValue){
+        foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")){
+            if(enemy.GetComponent<Enemy>().enemySequence[0] == buttonValue){
+                Debug.Log(enemy.name);
+                enemy.GetComponent<Enemy>().enemySequence.ToList().RemoveAt(0);
+            }
+        }
     }
 }
